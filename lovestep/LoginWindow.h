8d2d2b92567e9bf576adb29e78d15f641ae770d0
Raymond Kennedy <raymondk@stanford.edu>
2013-11-11 22:09:49 -0800

    Initial Login Window UI

---
+++ lovestep/LoginWindow.h
@@ -0,0 +1,13 @@
+//
+//  LoginWindow.h
+//  lovestep
+//
+//  Created by Raymond Kennedy on 11/11/13.
+//  Copyright (c) 2013 Zachary Waleed Saraf. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LoginWindow : NSWindow
+
+@end
