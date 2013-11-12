4796da29ac4998756fc77d11f746a9b1685196e6
Raymond Kennedy <raymondk@stanford.edu>
2013-11-11 22:38:08 -0800

    Login v1 complete ish fuck

--- lovestep/LoginWindow.m
+++ lovestep/LoginWindow.m
@@ -11,21 +11,56 @@
 @interface LoginWindow()
 
 @property (nonatomic, weak) IBOutlet NSTextField *usernameField;
+@property (nonatomic, weak) IBOutlet NSTextField *promptField;
+@property (nonatomic, weak) IBOutlet NSButton *continueButton;
+@property (nonatomic) BOOL hasFirstUsername;
 
 @end
 
 @implementation LoginWindow
 
 @synthesize usernameField = _usernameField;
+@synthesize promptField = _promptField;
+@synthesize continueButton = _continueButton;
+@synthesize hasFirstUsername = _hasFirstUsername;
 
 - (NSSize)windowWillResize:(NSWindow *) window toSize:(NSSize)newSize
 {
 	return [window frame].size; //no change
 }
 
-- (BOOL)control:(NSControl *)control textShouldBeginEditing:(NSText *)fieldEditor {
-    NSLog(@"Text changing...");
+- (BOOL)control:(NSControl *)control textShouldEndEditing:(NSText *)fieldEditor {
+    if ([[self.usernameField stringValue] isEqualToString:@""] || !self.usernameField) {
+        [self.usernameField setStringValue:@""];
+    }
+    
+    [self saveUsername];
+    
     return YES;
+}
+
+- (void)saveUsername {
+    if (!self.hasFirstUsername) {
+        [[NSUserDefaults standardUserDefaults] setValue:[self.usernameField stringValue] forKey:@"username"];
+        [self.continueButton setTitle:@"Finish"];
+        [self.usernameField setStringValue:@""];
+        self.hasFirstUsername = YES;
+        [self setupForPartner];
+    } else {
+        [[NSUserDefaults standardUserDefaults] setValue:[self.usernameField stringValue] forKey:@"partnerUsername"];
+        
+        // CONNECT THE SOCKET BITCH
+    }
+}
+
+- (IBAction)buttonPressed:(NSButton *)sender
+{
+    [self saveUsername];
+}
+
+- (void)setupForPartner {
+    [self.promptField setStringValue:@"Enter your partner's username:"];
+    
 }
 
 @end
