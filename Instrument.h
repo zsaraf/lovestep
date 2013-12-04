e22b0e066f4df60f2577ddfe217340bd74c22e45
Zachary Saraf <zsaraf@stanford.edu>
2013-12-03 20:36:46 -0800

    Added instrument subclass

---
+++ Instrument.h
@@ -0,0 +1,13 @@
+//
+//  Instrument.h
+//  lovestep
+//
+//  Created by Zachary Waleed Saraf on 12/3/13.
+//  Copyright (c) 2013 Zachary Waleed Saraf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Instrument : NSObject
+
+@end
---
+++ Instrument.m
@@ -0,0 +1,13 @@
+//
+//  Instrument.m
+//  lovestep
+//
+//  Created by Zachary Waleed Saraf on 12/3/13.
+//  Copyright (c) 2013 Zachary Waleed Saraf. All rights reserved.
+//
+
+#import "Instrument.h"
+
+@implementation Instrument
+
+@end
