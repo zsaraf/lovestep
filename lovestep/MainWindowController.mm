f995f4e43824db41427ae3110596e6bad168e77b
Zachary Saraf <zsaraf@stanford.edu>
2013-12-03 19:55:11 -0800

    got some sound working

--- lovestep/MainWindowController.mm
+++ lovestep/MainWindowController.mm
@@ -18,6 +18,7 @@
 @property (nonatomic, strong) AudioFileReader *fileReader;
 @property (nonatomic, strong) AudioFileWriter *fileWriter;
 @property (nonatomic, assign) RingBuffer *ringBuffer;
+@property (nonatomic) NSInteger counter;
 
 @end
 
@@ -40,20 +41,37 @@
     self.ringBuffer = new RingBuffer(32768, 2);
     
     __weak MainWindowController * wself = self;
-    __block int counter = 0;
-    
+    NSLog(@"%f", self.audioManager.samplingRate);
+    self.counter =0;
     [self.audioManager setOutputBlock:^(float *data, UInt32 numFrames, UInt32 numChannels)
      {
+         memset(data, 0, numFrames * numChannels * sizeof(float));
+         
+         // fill
+         for( int i = 0; i < numFrames; i++ )
+         {
+             // generate signal
+             data[i*numChannels] = ::sin( 2 * M_PI * 880 * wself.counter / wself.audioManager.samplingRate);
+             
+             // copy into other channels
+             for( int j = 1; j < numChannels; j++ )
+                 data[i*numChannels+j] = data[i*numChannels];
+             // increment sample number
+             wself.counter++;
+         }
+         
+//         int mod = wself.counter *numFrames % (int)wself.audioManager.samplingRate;
+//         if (mod < numFrames) {
+//             for (int i = 0; i < numChannels; i++) data[mod *numChannels + i] = 1;
+//             NSLog(@"In");
+//         }
 //         for (int i = 0; i < numFrames; i++) {
 //             data[i * numChannels] = rand() % (32768 * 2) - 32768;
 //             
 //             for (int j = 1; j < numChannels; j++) data[i * numChannels + j] = data[i * numChannels];
 //         }
          
-         [wself.fileReader retrieveFreshAudio:data numFrames:numFrames numChannels:numChannels];
-         counter++;
-         if (counter % 80 == 0)
-             NSLog(@"Time: %f", wself.fileReader.currentTime);
+         //[wself.fileReader retrieveFreshAudio:data numFrames:numFrames numChannels:numChannels];
      }];
     
     [self.audioManager play];
