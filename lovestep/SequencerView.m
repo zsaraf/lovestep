b6947eb31f4c522e0142c9521999817961a3163b
Zachary Saraf <zsaraf@stanford.edu>
2013-12-10 19:58:24 -0800

    Added beginning of enter key

--- lovestep/SequencerView.m
+++ lovestep/SequencerView.m
@@ -249,6 +249,13 @@ typedef struct Resolution {
     [self highlightColumn:noteNumber];
 }
 
+-(void)keyDown:(NSEvent *)theEvent
+{
+    if (theEvent.keyCode == 36) {
+        
+    }
+}
+
 /*
  * Gets the key number at the given index
  */
