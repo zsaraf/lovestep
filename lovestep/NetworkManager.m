718846368a2543fc61b3f0985f6b69d941499077
Zachary Saraf <zsaraf@stanford.edu>
2013-12-10 23:41:36 -0800

    dedknedkd

--- lovestep/NetworkManager.m
+++ lovestep/NetworkManager.m
@@ -8,7 +8,9 @@
 
 #import "NetworkManager.h"
 
-#define WAITING_FOR_OTHER_USER 20
+#define WAITING_FOR_OTHER_USER_TAG 20
+#define USER_NAME_SEND_TAG 21
+#define USER_NAME_RECEIVE_TAG 22
 
 @interface NetworkManager ()
 
@@ -37,14 +39,21 @@ static NetworkManager *myInstance;
     if (self.asyncSocket) return;
     self.asyncSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
     [self.asyncSocket connectToHost:@"10.31.78.119" onPort:80 error:nil];
-    [self.asyncSocket readDataWithTimeout:20 tag:WAITING_FOR_OTHER_USER];
+    [self.asyncSocket readDataWithTimeout:20 tag:WAITING_FOR_OTHER_USER_TAG];
 }
 
 -(void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
 {
-    if (tag == WAITING_FOR_OTHER_USER) {
+    if (tag == WAITING_FOR_OTHER_USER_TAG) {
         NSLog(@"%@", [NSString stringWithUTF8String:[data bytes]]);
         [self.delegate networkManagerDidFindNetworkService:YES];
+        NSData *data = [[[NSUserDefaults standardUserDefaults] valueForKey:@"username"] dataUsingEncoding:NSUTF8StringEncoding];
+        [self.asyncSocket writeData:data
+                        withTimeout:3
+                                tag:USER_NAME_SEND_TAG];
+        [self.asyncSocket readDataWithTimeout:4 tag:USER_NAME_RECEIVE_TAG];
+    } else if (tag == USER_NAME_RECEIVE_TAG) {
+        NSLog(@"%@", [NSString stringWithUTF8String:[data bytes]]);
     }
 }
 
