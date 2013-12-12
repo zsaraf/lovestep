f01eabbf4ea822e7f00878f3cbffe3814e98f2b0
Zachary Saraf <zsaraf@stanford.edu>
2013-12-11 17:36:16 -0800

    Added some loop drawing features

--- lovestep/LoopView.m
+++ lovestep/LoopView.m
@@ -14,7 +14,9 @@
 - (id)initWithFrame:(NSRect)frame andLoop:(Loop *)loop
 {
     if (self = [super initWithFrame:frame]) {
+        self.layer.cornerRadius = 10;
         self.loop = loop;
+        
     }
     
     return self;
@@ -22,9 +24,21 @@
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-    [[NSColor colorWithCalibratedRed:.4f green:.4f blue:.6f alpha:1.0f] setFill];
-    NSRectFill(dirtyRect);
+    NSRect rect = NSMakeRect([self bounds].origin.x + 3, [self bounds].origin.y + 3, [self bounds].size.width - 6, [self bounds].size.height - 6);
+    
+    NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:rect xRadius:10.0 yRadius:10.0];
+    [path addClip];
+    
+    if ([self.loop.creator isEqualToString:[[NSUserDefaults standardUserDefaults] valueForKey:@"username"]]) {
+        [[NSColor colorWithDeviceRed:134/255. green:202/255. blue:254/255. alpha:1] set];
+    } else {
+        [[NSColor colorWithDeviceRed:162/255. green:134/255. blue:254/255. alpha:1] set];
+    }
+    NSRectFill(rect);
     [super drawRect:dirtyRect];
+    [[NSColor colorWithDeviceRed:0 green:0 blue:0 alpha:1.] set];
+    [path setLineWidth:5];
+    [path stroke];
 }
 
 @end
